687f0558db76c4d7f9966d9fff34f91695b7cc72
wus <swunderlin@gmail.com>
2013-09-26 18:34:55 +0200

    stripped down version

--- ArduMavProxy/ArduMavProxy.ino
+++ ArduMavProxy/ArduMavProxy.ino
@@ -33,69 +33,67 @@
 //#include <SoftwareSerial.h>
 #include <BetterSoftwareSerial.h>
 BetterSoftwareSerial MavPortAPM(10, 11); // TX, RX
-BetterSoftwareSerial MavPortModem(2, 3); // TX, RX
+BetterSoftwareSerial MavPortModem(8, 9); // TX, RX
 
 // Configurations
 #include "vars.h"
 
 #define TELEMETRY_SPEED  57600  // How fast our MAVLink telemetry is coming to Serial port
-#define BOOTTIME         2000   // Time in milliseconds that we show boot loading bar and wait user input
+#define BOOTTIME		 2000   // Time in milliseconds that we show boot loading bar and wait user input
 
 // Objects and Serial definitions
 //FastSerialPort0(Serial);
 
 SimpleTimer  mavlinkTimer;
 
-void setup() 
-{
-#ifdef ArduCAM328
-    pinMode(10, OUTPUT); // USB ArduCam Only
-#endif
-
-    //Serial.begin(TELEMETRY_SPEED);
-    // setup mavlink port
-    //mavlink_comm_0_port = &Serial;
-    mavlink_comm_0_port = &MavPortModem;
+void setup() {
+	Serial.begin(TELEMETRY_SPEED);
+	// setup mavlink port
+	//mavlink_comm_0_port = &Serial;
+	mavlink_comm_0_port = &MavPortModem;
+	
+	MavPortAPM.begin(TELEMETRY_SPEED);
+	MavPortModem.begin(TELEMETRY_SPEED);
 
 #ifdef membug
-    Serial.println(freeMem());
+	Serial.println(freeMem());
 #endif
 
-    //delay(500);
+	//delay(500);
 
-    // Startup MAVLink timers  
-    mavlinkTimer.Set(&OnMavlinkTimer, 120);
+	// Startup MAVLink timers  
+	mavlinkTimer.Set(&OnMavlinkTimer, 120);
 
-    mavlinkTimer.Enable();
+	mavlinkTimer.Enable();
 
 } // END of setup();
 
 
-void loop() 
-{
+void loop() {
 
-    if(enable_mav_request == 1){//Request rate control
-        for(int n = 0; n < 3; n++){
-            request_mavlink_rates();//Three times to certify it will be readed
-            delay(50);
-        }
-        enable_mav_request = 0;
-        delay(2000);
-        waitingMAVBeats = 0;
-        lastMAVBeat = millis();//Preventing error from delay sensing
-    }
+	if(enable_mav_request == 1){//Request rate control
+		for(int n = 0; n < 3; n++){
+			request_mavlink_rates();//Three times to certify it will be read
+			delay(50);
+		}
+		enable_mav_request = 0;
+		delay(2000);
+		waitingMAVBeats = 0;
+		lastMAVBeat = millis();//Preventing error from delay sensing
+	}
 
-    read_mavlink();
-    mavlinkTimer.Run();
+	read_mavlink();
+	mavlinkTimer.Run();
 }
 
 /* *********************************************** */
 /* ******** functions used in main loop() ******** */
 void OnMavlinkTimer() {
-	;
+	Serial.print("apm_mav_system ");
+	Serial.println(apm_mav_system);
 }
 
 /// Legacy, remove?
-void unplugSlaves(){
+void unplugSlaves() {
 	;
 }

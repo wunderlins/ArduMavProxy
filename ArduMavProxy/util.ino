bc77b25d1ce67308e7f236bbca81ad86f36eba55
wus <swunderlin@gmail.com>
2013-09-29 18:00:11 +0200

    documentation

--- ArduMavProxy/util.ino
+++ ArduMavProxy/util.ino
@@ -15,25 +15,16 @@ byte setBit(byte &Reg, byte whichBit, boolean stat) {
     }
     return Reg;
 }
-/*
-uint8_t read_packet_old(mavlink_message_t *msg, 
-                 mavlink_status_t *status, 
-                 HardwareSerial *source, 
-                 HardwareSerial *target) {
-	//grabing data 
-	while(source->available() > 0) { 
-		uint8_t c = source->read();
-		target->write(c);
 
-		//trying to grab msg  
-		if(mavlink_parse_char(MAVLINK_COMM_0, c, msg, status)) {
-			return 1;
-		}
-	}
-	return 0;
-}
-*/
-
+/**
+ * read a mavlink packet
+ *
+ * returns 1 if we got a complete packet. returns 0 if we need to read 
+ * more into the stream.
+ * 
+ * passthrough is for minimal latency. Best used when only sniffing
+ *
+ */
 uint8_t read_packet(comm_t *src, comm_t *target, bool passthrough) {
 	//grabing data 
 	while(src->serial->available() > 0) { 
